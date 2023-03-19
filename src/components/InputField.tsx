8a0358e854e680df00d5f02864637484632e5a69
Aziz <azizahmed.dev@gmail.com>
2023-03-19 19:21:07 -0400

    typescript tasklist

---
+++ src/components/InputField.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+import { useRef } from 'react';
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({todo, setTodo, handleAdd} : Props) => {
+  
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form className='input' onSubmit={(e) => {handleAdd(e);
+      inputRef.current?.blur();}}
+    >
+      <input 
+        type='input' placeholder='enter a task' className='input__box'
+        ref={inputRef}
+        value={todo} onChange={(e) => setTodo(e.target.value)}
+      />
+      <button className='input__submit' type="submit"><span>Add</span></button>
+    </form>
+  )
+}
+
+export default InputField
